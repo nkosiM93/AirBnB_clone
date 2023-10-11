e0fe1c8f96a431c4eeeb867de1f3dfbc0e345dd6
AlphaCoder <mathiyathokozanin@gmail.com>
2023-10-11 09:46:35 -0700

    Question 5 Fix: reload(), save(), new()

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """File storage module"""
 import json
-
+from models.base_model import BaseModel
 
 class FileStorage:
     """class representation"""
@@ -10,27 +10,26 @@ class FileStorage:
 
     def all(self):
         """returns dictionary """
-        dReload = self.reload()
-        if not dReload or dReload is None:
-            pass
-        else:
-            for k, v in dReload.items():
-                self.__objects[k] = v
         return self.__objects
 
     def new(self, obj):
         """sets in objects"""
         k = f"{obj.__class__.__name__}.{obj.id}"
-        self.__objects[k] = obj.to_dict()
+        self.__objects[k] = obj
 
     def save(self):
+        __tempD = {}
+        for k,v in self.__objects.items():
+            __tempD[k] = v.to_dict()
         with open(self.__file_path, "w", encoding="utf-8") as w_file:
-            json.dump(self.__objects, w_file)
+            json.dump(__tempD, w_file)
 
     def reload(self):
         """deserializes the JSON file to __objects"""
         try:
             with open(self.__file_path, "r", encoding="utf-8") as r_file:
-                return json.load(r_file)
+                __tempD = json.load(r_file)
+                for v in __tempD.values():
+                    BaseModel(v)
         except(FileNotFoundError):
             pass
